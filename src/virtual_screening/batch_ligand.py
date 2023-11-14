86b581ed1b2145d2866cddee7fe1ea8eb55090ed
FEI YUAN <fei.yuan@bcm.edu>
2023-11-14 12:22:57 -0600

    Replaced --batch option with fixed record number 3000 for batching

--- src/virtual_screening/batch_ligand.py
+++ src/virtual_screening/batch_ligand.py
@@ -21,8 +21,6 @@ parser = argparse.ArgumentParser(prog='batch-ligand', description=__doc__.strip(
 parser.add_argument('ligand', help="Path to a single SDF file contains prepared ligands", type=vstool.check_file)
 parser.add_argument('pdb', help="Path to the receptor in PDB format file", type=vstool.check_file)
 parser.add_argument('--outdir',help="Path to a directory for saving output files", type=vstool.mkdir)
-parser.add_argument('--batch', type=int, help="Number of batches that the SDF will be split to, "
-                                                    "default: %(default)s", default=8)
 parser.add_argument('--center', help="The X, Y, and Z coordinates of the center",
                     type=float, nargs='+', required=True)
 
@@ -38,8 +36,8 @@ vstool.setup_logger(verbose=True)
 
 
 def main():
-    logger.debug(f'Splitting {args.ligand} into {args.batch} batches ...')
-    batches = MolIO.batch_sdf(args.ligand, args.batch, args.outdir / 'batch.')
+    logger.debug(f'Splitting {args.ligand} into batches ...')
+    batches = MolIO.split_sdf(args.ligand, args.outdir / 'batch.', records=3000)
     logger.debug(f'Successfully split {args.ligand} into {len(batches)} batches ...')
 
     (cx, cy, cz), (sx, sy, sz), cmds = args.center, args.size, []
@@ -62,7 +60,7 @@ def main():
             cmds.append(cmd)
         cmds = cmds[:8] if args.debug else cmds
         o.writelines(f'{cmd}\n' for cmd in cmds)
-    logger.debug(f'Successfully saved the following {len(cmds)} into {output}')
+    logger.debug(f'Successfully saved the following {len(cmds)} docking commands into {output}')
 
 
 if __name__ == '__main__':
